718723f15a2e8e45cce94e6d0117e802037f085f
Jeongmin <srm0609@naver.com>
2023-07-06 15:14:38 +0900

    #2.7 Location

--- NomardWeather/App.js
+++ NomardWeather/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const granted = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    console.log(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={style.container}>
       <View style={style.city}>
-        <Text style={style.cityName}>Seoul</Text>
+        <Text style={style.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
